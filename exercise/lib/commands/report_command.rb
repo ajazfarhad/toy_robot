d0065d5a797b25dc3666e27d6d55badb7790fb60
Farhad Ajaz <ajaz.farhad19@gmail.com>
2024-12-04 07:24:26 +0000

    remove unnecessary directory

--- exercise/lib/commands/report_command.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Command to report the robot's current position and direction.
-#
-class ReportCommand < Command
-  def execute(robot)
-    puts robot.report
-  end
-end
